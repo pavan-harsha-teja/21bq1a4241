ae5db9979150aaf76076a9247c0ca2295ef28bbf
pavan-harsha-teja <113110492+pavan-harsha-teja@users.noreply.github.com>
2022-10-14 08:56:48 +0530

    Add files via upload

---
+++ linked list 41.py
@@ -0,0 +1,17 @@
+class node:
+    def __init__(self):
+        self.data=data
+        self.ref=none
+class linkedlist:
+    def __init__(self):
+        self.head=None
+    def print_ll(Self):
+        if self.head is None:
+            print("linked list is empty")
+        else:
+            n=self.head
+            while n is not none:
+                print(n.data)
+                h=n.ref
+ll1=linkedlist()
+ll1.print_ll()
